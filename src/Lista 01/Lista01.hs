c1954c21dab0b0164afbb9d892f0914a469bcd41
Wellington Schütz <wellington.schu@live.com>
2016-06-16 11:30:36 -0300

    Implementação Lista de Exercicios Exercicios 1 ao 4

---
+++ src/Lista 01/Lista01.hs
@@ -0,0 +1,12 @@
+module Lista01 where
+--1
+quadrado x  = x^2
+
+--2
+somaTresNumeros x y z = x + y + z
+
+--3
+metadeMenorQueDez x = if x < 10 then x/2 else x
+
+--4
+parImpar x = if mod x 2 == 0 then "Par" else "Impar"
