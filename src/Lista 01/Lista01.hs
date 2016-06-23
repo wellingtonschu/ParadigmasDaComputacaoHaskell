81335f930840e135f2a0d2869669ac8336d22b55
Wellington Schütz <wellington.schu@live.com>
2016-06-23 10:15:57 -0300

    Implementação Lista de Exercicios Exercicios 5 ao 10

--- src/Lista 01/Lista01.hs
+++ src/Lista 01/Lista01.hs
@@ -10,3 +10,20 @@ metadeMenorQueDez x = if x < 10 then x/2 else x
 
 --4
 parImpar x = if mod x 2 == 0 then "Par" else "Impar"
+
+--5
+quadradoLista lista = (head lista)^2
+qudradoLista2 lista =  (lista!!0)^2
+
+--6
+notaConceito nota = if nota>=9 then "Conceito A"
+					else if nota>=7.5 && nota<9 then "Conceito B"
+					else if nota>=6 && nota<7.5 then "Conceito C"
+					else if nota>=4 && nota<6 then "Conceito D"
+					else "Conceito E"
+
+-- 7
+somaLista lista =  sum lista
+
+--9
+senhorOuSenhora x y = if y == 'M' || y == 'm' then "Sr. "++x then "Sra. "++x
